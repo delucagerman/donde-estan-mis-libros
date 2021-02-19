8c8ff22a375887b81095adc662092de392cf3197
Julian Zambroni <julian.zambroni@gmail.com>
2021-02-19 12:33:20 -0300

    Pequeo fix en typeo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const app = require("./app");
 
 const PORT = process.env.PORT || 3001;
@@ -6,13 +5,3 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Escuchando en puerto ${PORT}`);
 });
-=======
-const app = require('./app');
-
-
-const PORT = process.env.PORT || 3000;
-//CONEXION A PUERTO
-app.listen(PORT, () => {
-    console.log(`Escuchando en puerto ${PORT}`);
-});
->>>>>>> ebf692f8f7d3b63ea1d65285b9f8a7db1492a2d0
